be292e07e27ebb52b2ec0c12dd1cc9bafe8c02c5
Fionn McCarthy <fionntmcc04@gmail.com>
2026-08-05 14:37:35 +0100

    Define message and server struct.

--- message.go
+++ message.go
@@ -1,1 +1,9 @@
 package chat_server
+
+import "time"
+
+type Message struct {
+	sender string
+	text   string
+	time   time.Time
+}
--- server.go
+++ server.go
@@ -1,1 +1,13 @@
 package chat_server
+
+import (
+	"net"
+	"sync"
+)
+
+type Server struct {
+	listener  net.Listener
+	clients   map[net.Conn]string // connection -> username
+	mu        sync.RWMutex
+	broadcast chan []byte
+}
